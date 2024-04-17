c23e8608987c2f62b22e9bd9828e89bd08491e2e
ErdajtSopjani <erdajt.sopjani@bgt.school>
2024-04-17 09:43:56 +0200

    Changed default theme to everforest

--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -26,6 +26,7 @@ return {
       vim.g.everforest_enable_italic = 1
       vim.g.everforest_better_performance = 1
       vim.g.everforest_cursor = 'aqua'
+      vim.cmd [[colorscheme everforest]]
     end,
   },
 
