0f766a9a6bc1953bdb3ce273d80798c1938ba065
ErdajtSopjani <erdajt.sopjani@bgt.school>
2024-05-02 01:47:32 +0200

    Added clang-format for cpp files

--- lua/plugins/configs/lspconfig.lua
+++ lua/plugins/configs/lspconfig.lua
@@ -143,6 +143,7 @@ function M.setup()
   local ensure_installed = vim.tbl_keys(servers or {})
   vim.list_extend(ensure_installed, {
     'stylua', -- Used to format lua code
+    'clang-format',
   })
   require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
--- lua/plugins/utils.lua
+++ lua/plugins/utils.lua
@@ -63,6 +63,7 @@ return {
       end,
       formatters_by_ft = {
         lua = { 'stylua' },
+        c = { 'clang-format' },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
         -- You can use a sub-list to tell conform to run *until* a formatter
