b49e636caccee76365451e92b141655bfc04b5a9
ErdajtSopjani <erdajt.sopjani@bgt.school>
2024-05-02 01:48:37 +0200

    Actually added clang-format for cpp files

--- lua/plugins/utils.lua
+++ lua/plugins/utils.lua
@@ -64,6 +64,7 @@ return {
       formatters_by_ft = {
         lua = { 'stylua' },
         c = { 'clang-format' },
+        cpp = { 'clang-format' },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
         -- You can use a sub-list to tell conform to run *until* a formatter
