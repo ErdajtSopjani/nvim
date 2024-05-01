6e3c95f6d72a14de3622fb56652952ce89db1ff3
ErdajtSopjani <erdajt.sopjani@bgt.school>
2024-05-01 21:34:33 +0200

    Disabled crursorline

--- lua/settings.lua
+++ lua/settings.lua
@@ -52,6 +52,6 @@ vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
 -- Preview substitutions live, as you type!
 vim.opt.inccommand = 'split'
 -- Show which line your cursor is on
-vim.opt.cursorline = true
+-- vim.opt.cursorline = true
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 10
