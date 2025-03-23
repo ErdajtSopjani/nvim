b6226be088c591f93273cc4348a1a30fdbb65c17
ErdajtSopjani <erdajtsopjani.tech@gmail.com>
2025-03-24 00:29:00 +0100

    new colorscheme

--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -9,6 +9,13 @@ return {
 	},
 
 	{
+		"EdenEast/nightfox.nvim",
+		config = function()
+			vim.cmd.colorscheme("nightfox")
+		end,
+	},
+
+	{
 		"folke/tokyonight.nvim",
 		-- init = function()
 		-- vim.cmd 'set termguicolors'
@@ -22,15 +29,15 @@ return {
 
 	{
 		"sainnhe/everforest",
-		config = function()
-			vim.g.everforest_background = "hard"
-			vim.g.everforest_terminal_colors = 1
-			vim.g.everforest_enable_italic = 1
-			vim.g.everforest_better_performance = 1
-			vim.g.everforest_cursor = "aqua"
-			vim.cmd.colorscheme("everforest")
-			-- can set warning/error text to colored: vim.g.everforest_diagnostic_virtual_text = 'colored'
-		end,
+		-- config = function()
+		-- 	vim.g.everforest_background = "hard"
+		-- 	vim.g.everforest_terminal_colors = 1
+		-- 	vim.g.everforest_enable_italic = 1
+		-- 	vim.g.everforest_better_performance = 1
+		-- 	vim.g.everforest_cursor = "aqua"
+		-- 	vim.cmd.colorscheme("everforest")
+		-- 	-- can set warning/error text to colored: vim.g.everforest_diagnostic_virtual_text = 'colored'
+		-- end,
 	},
 
 	{
